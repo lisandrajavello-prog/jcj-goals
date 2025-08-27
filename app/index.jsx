1c40555fcb4fa2b264b3e67ef51da6bd74a102dc
jeduuujed <jeddelacerna1@gmail.com>
2025-08-27 15:38:04 +0800

    index is changed

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
-        J C J  G O A L S
+        J C J  G O A L S TrackeristaBalibolista
       </Text>
       <Link style={styles.link} href="/goals">
         View Your Goals
