e18edce5494d6e4307cfcba5a06d7e714c794bb3
jeduuujed <jeddelacerna1@gmail.com>
2025-08-27 15:34:13 +0800

    Firebase Configuration

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyDNyWXHLqrrdmVEhfPY559HOMSKW4ScTQ8",
+  authDomain: "ed-tech-2004.firebaseapp.com",
+  projectId: "ed-tech-2004",
+  storageBucket: "ed-tech-2004.firebasestorage.app",
+  messagingSenderId: "393271296491",
+  appId: "1:393271296491:web:d65f16c04afe5c539519ae"
+};
+
+
+const app = initializeApp(firebaseConfig);
